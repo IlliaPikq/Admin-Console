7c0f7b893221da7a4d85f7fea90a925954f3676d
Illia <illiapikhotskyi@gmail.com>
2023-07-19 00:11:41 +0200

    theme switch && static files serve added

--- server.ts
+++ server.ts
@@ -1,9 +1,17 @@
-import { indexOfNeedle } from 'https://deno.land/std@0.193.0/bytes/index_of_needle.ts';
 import { Application, Router } from 'https://deno.land/x/oak/mod.ts';
 import { send } from 'https://deno.land/x/oak@v12.6.0/send.ts';
 
 const app = new Application();
 const router = new Router();
+
+app.use(async (ctx, next) => {
+	const path = ctx.request.url.pathname;
+
+	await send(ctx, path, {
+		root: `${Deno.cwd()}/public/`,
+	});
+	await next();
+});
 
 router.get('/', async (req) => {
 	req.response.headers.set('Content-Type', 'text/html');
