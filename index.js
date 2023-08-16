f3565eac1412b0c2289904e31b3d1f933e343b4c
Illia <illiapikhotskyi@gmail.com>
2023-08-16 14:16:17 +0200

    changes

--- index.js
+++ index.js
@@ -4,13 +4,7 @@ const fs = require('fs');
 const app = express();
 
 app.use(cors());
-app.use((req, res, next) => {
-  res.setHeader(
-    'Content-Security-Policy',
-    "default-src 'none'; script-src 'self' https://vercel.live 'unsafe-inline';"
-    );
-    next();
-  });
+
 app.use(express.json());
   
   
